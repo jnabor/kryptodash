ec6a0b3e377cc6ed868d627326a9f5119face913
sonab <sonabstudios@gmail.com>
2019-11-09 16:45:44 +0800

    coin tile favorites: add, remove, disable

---
+++ build/precache-manifest.4d1c90719e853dee647e8ac7324cc4cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb9008b9e2fa0ef2e2d359319090502",
+    "url": "/kryptodash/index.html"
+  },
+  {
+    "revision": "951f970819dede8a592e",
+    "url": "/kryptodash/static/css/main.bf34527f.chunk.css"
+  },
+  {
+    "revision": "020cfa711ebd1e1ee34b",
+    "url": "/kryptodash/static/js/2.7f27fa15.chunk.js"
+  },
+  {
+    "revision": "951f970819dede8a592e",
+    "url": "/kryptodash/static/js/main.67c3c2be.chunk.js"
+  },
+  {
+    "revision": "f0562631c2375588d8c1",
+    "url": "/kryptodash/static/js/runtime-main.25aed29c.js"
+  }
+]);
